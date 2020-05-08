87bba633c3fd4c9fd1e0462ae0995a7182cd4cc3
MFdesigns <michel.faeh@stafag.ch>
2020-05-08 16:36:14 +0200

    Setup commit

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!--
+  Copyright (c) 2020 Michel Fäh, Dario Romandini
+  -->
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Michel Fäh, Dario Romandini">
+  <!-- TODO: Add description meta data -->
+  <meta name="description" content="">
+  <title>OGD Lernende Mittelschulen TG</title>
+</head>
+<body>
+
+</body>
+</html>
