153308a769f24482022616cf15ba8307e58dc472
MFdesigns <michel.faeh@stafag.ch>
2020-05-10 18:24:49 +0200

    Converted tabs to spaces and added file header

--- src/api.php
+++ src/api.php
@@ -1,10 +1,16 @@
 <?php
+
+/*
+  Copyright (c) 2020 Dario Romandini
+*/
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-	$json = file_get_contents('../data/dataset.json');
-	header('Content-Type: application/json; charset=UTF-8');
-	echo $json;
+  $json = file_get_contents('../data/dataset.json');
+  header('Content-Type: application/json; charset=UTF-8');
+  echo $json;
 } else {
-	http_response_code(405);
-	die();
+  http_response_code(405);
+  die();
 }
+
 ?>
